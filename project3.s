8f4016174e891ce45e78221b95afa59251e40f7d
Suprabhat Rijal <goodguysman@gmail.com>
2022-12-04 11:40:42 -0500

    Get user input

--- project3.s
+++ project3.s
@@ -3,3 +3,8 @@ input: .space 1001
 .text
 ############################# main program starts #######################################
 main:
+# get input string from the user
+li $v0, 8
+la $a0, input
+li $a1, 1001
+syscall
